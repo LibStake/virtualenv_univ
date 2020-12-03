1189af379069c97531b998493243bac020f4d4fd
Dminor95 <asqwaqsw@naver.com>
2020-12-04 04:28:07 +0900

    Piano changed

---
+++ Assets/Music_Notes/MusicNoteCodes.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicNoteCodes : MonoBehaviour
+{
+    private void Start()
+    {
+
+
+    }
+    void Update()
+    {
+
+    }
+
+
+    public AudioSource C_Note;
+    public AudioSource D_Note;
+    public AudioSource E_Note;
+    public AudioSource F_Note;
+    public AudioSource G_Note;
+    public AudioSource A_Note;
+    public AudioSource B_Note;
+    public AudioSource C1_Note;
+    public AudioSource Twinkle_Note;
+
+    public void C_Note_Play()
+    {
+        C_Note.Play();
+    }
+    public void D_Note_Play()
+    {
+        D_Note.Play();
+    }
+    public void E_Note_Play()
+    {
+        E_Note.Play();
+    }
+    public void F_Note_Play()
+    {
+        F_Note.Play();
+    }
+    public void G_Note_Play()
+    {
+        G_Note.Play();
+    }
+    public void A_Note_Play()
+    {
+        A_Note.Play();
+    }
+    public void B_Note_Play()
+    {
+        B_Note.Play();
+    }
+    public void C1_Note_Play()
+    {
+        C1_Note.Play();
+    }
+    public void Twinkle_Note_Play()
+    {
+        Twinkle_Note.Play();
+    }
+}
