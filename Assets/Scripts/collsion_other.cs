161e362c73b6efb90cc826b81b6f7ce0ad4d805e
gwono52 <75409431+gwono52@users.noreply.github.com>
2020-12-04 17:52:16 +0900

    update throwball

---
+++ Assets/Scripts/collsion_other.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collsion_other : MonoBehaviour
+{
+    GameObject portal;
+    Transform target;
+    // Start is called before the first frame update
+
+    void Awake() {
+        portal = GameObject.Find("exitportal");
+        target = portal.GetComponent<Portal>().target;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+        if (hit.gameObject == portal)
+        {
+            Debug.Log("portal_on");
+            transform.position = target.position;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
