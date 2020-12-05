512a6fea310ed74259c4a3ef99e5c00edecd0f12
GipyoChoi <polling.choi@gmail.com>
2020-12-05 19:10:31 +0900

    Init game clear logic

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    // 게임 클리어 하는법
+    // GameObject.Find("Controller").GetComponent<GameMaster>().setQuizClear(); 실행
+
+    public int numToClearGame = 1;
+    private int gameCleared = 0;
+
+    public void setQuizClear()
+    {
+        gameCleared += 1;
+
+        if (numToClearGame == gameCleared)
+        {
+            setGameClear();  
+        }
+        
+    }
+
+    private void setGameClear()
+    {
+        // Game cleared
+    }
+}
