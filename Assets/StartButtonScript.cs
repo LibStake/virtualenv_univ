27f2cb34ebc5d5a4fdd9b73b0cf7d9317ee18102
gwono52 <75409431+gwono52@users.noreply.github.com>
2020-12-05 20:02:22 +0900

    add throwball2

---
+++ Assets/StartButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonScript : MonoBehaviour
+{
+    public GameObject startCanvas;
+    // Start is called before the first frame update
+
+   public void onStart()
+    {
+        startCanvas.SetActive(false);
+    }
+}
